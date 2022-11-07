f5382a7849f022e9746df5da3a8d7ac738648998
alicia4645 <112639409+alicia4645@users.noreply.github.com>
2022-11-07 09:59:59 +0000

    Update server.java

--- server.java
+++ server.java
@@ -13,18 +13,15 @@ public class server{
             try{
        
                 DataInputStream input =new DataInputStream(clientSocket.getInputStream());
-                // DataOutputStream output=new DataOutputStream(clientSocket.getOutputStream());
+               
                 String str=(String)input.readUTF();
-                //when client types exit stop server connection else print message
+              
                 if(str.equals("stop")){
                     break;
                 }
                 else{
                     System.out.println(str);
                 }
-
-             //Thread myThread = new thread(ClientSocket, input, output);
-                // myThread.start();
             }
             catch(Exception e){ 
                 server.close();
